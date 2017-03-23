f14cf07851e228ab49f0489b1c1e262404de5631
sam <sam.cottrell@network.rca.ac.uk>
2017-03-23 18:57:00 +0000

    D3 now working

--- imports/ui/study.js
+++ imports/ui/study.js
@@ -3,25 +3,12 @@ import { Template } from 'meteor/templating';
 import { ReactiveDict } from 'meteor/reactive-dict';
  
 import './study.html';
-import './timeline.js';
+// import './timeline.js';
 
-console.log("balls to the wall")
 
 Template.study.onRendered(function () {
 
-    console.log("shit loaded yo")
-
-    d3.select("#chart")
-        .append("svg")
-        .attr("width", 200)
-        .attr("height", 200)
-            .append("circle")
-            .attr("cx", 100)
-            .attr("cy", 100)
-            .attr("r", 20)
-            .attr("fill", "red");
-
-    import './timeline.js';
+     import './timeline.js';
 
 });
 
