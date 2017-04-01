f0b65a6a507ec88e66e4544f03b3019cf0ed459f
Sam <sam.cottrell@network.rca.ac.uk>
2017-04-01 22:12:53 +0100

    Getting the login working

--- imports/ui/signup.js
+++ imports/ui/signup.js
@@ -11,34 +11,58 @@ Template.signup.onRendered(function () {
 });
 
 if (Meteor.isClient) {
-    Template.signup.events({
-        'submit form': function(event){
-            event.preventDefault();
-            var emailVar = event.target.registerEmail.value;
-            var passwordVar = event.target.registerPassword.value;
-
-            Accounts.createUser({
-                email: emailVar,
-                password: passwordVar
-            });
-        }
-    });
 
 
     Template.login.events({
         'submit form': function(event) {
             event.preventDefault();
-            var emailVar = event.target.loginEmail.value;
+            var usernameVar = event.target.loginUsername.value;
             var passwordVar = event.target.loginPassword.value;
 
-            Meteor.loginWithPassword(emailVar, passwordVar);
+            Meteor.loginWithPassword(usernameVar, passwordVar);
         }
     });
+
+
+    Template.signup.events({
+        'submit form': function(event){
+            event.preventDefault();
+            var usernameVar = event.target.registerUsername.value;
+            var passwordVar = event.target.registerPassword.value;
+            var nameVar = event.target.name.value;
+            var emailVar = event.target.registerEmail.value;
+            var genderVar = event.target.sex.value;
+            var ageVar = event.target.age.value;
+            var occupationVar = event.target.occupation.value;
+            var routeVar = event.target.route.value;
+            var tnaVar = event.target.tna.value;
+            var tnaDeptVar = event.target.tnaDept.value;
+
+            Accounts.createUser({
+                username: usernameVar,
+                password: passwordVar,
+                name: nameVar,
+                email: emailVar,
+                gender: genderVar,
+                age: ageVar,
+                occupation: occupationVar,
+                route: routeVar,
+                tna: tnaVar,
+                tnaDept: tnaDeptVar 
+
+            });
+        }
+    });
+
+
 
 
     Template.dashboard.events({
         'click .logout': function(event){
             event.preventDefault();
+
+            Meteor.logout();
+
         }
     });
 
