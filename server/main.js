fb78e784b0abf72d4bb342c9f558bcd6f58e3d2b
sam <sam.cottrell@network.rca.ac.uk>
2017-04-06 11:05:18 +0100

    making the tasks clearer

--- server/main.js
+++ server/main.js
@@ -4,7 +4,7 @@ import { studyEntry } from '../imports/api/collection.js';
 
 Meteor.methods({
   insertData(data) {
-    if(!Meteor.userId()) throw new Meteor.Error(403, 'Unauthenticated');
+    if(!this.userId()) throw new Meteor.Error(403, 'Unauthenticated');
     data.IP = this.connection.clientAddress;
     data.user = this.user().username;
     data.userID = this.userId();
@@ -14,6 +14,8 @@ Meteor.methods({
 });
 
 Meteor.publish('studies', function () {
-  if(Meteor.userId() !== "H3wws8zn87Hn2Gw9c") return [];
+  if(this.userId() !== "H3wws8zn87Hn2Gw9c") return [];
   return studyEntry.find();
 });
+
+//my id live = H3wws8zn87Hn2Gw9c
